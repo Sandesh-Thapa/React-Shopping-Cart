2dc0cee2f78cfd4e2fd1d9c773e603351ca6e2d4
Sandesh-Thapa <sandeshthapa426@gmail.com>
2020-10-23 09:43:22 +0545

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // feature 1
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'
 
@@ -11,7 +12,29 @@ class App extends React.Component {
       products: data.products,
       size: '',
       sort: '',
-    }
+    };
+  }
+  sortProducts = (e) => {
+    const sort = e.target.value;
+    this.setState(state => ({
+      sort: sort,
+      products: state.products.slice().sort((a,b) => (
+        sort === "lowest" ? ((a.price > b.price) ? 1 : -1):
+        sort === "highest" ? ((a.price < b.price) ? 1 : -1):
+        ((a._id > b._id) ? 1 : -1)
+      ))
+    }))
+  }
+
+  filterProducts = (e) => {
+    if(e.target.value === ""){
+      this.setState({size:e.target.value, products:data.products})
+    } else {
+    this.setState({
+      size: e.target.value,
+      products: data.products.filter(product => product.availableSizes.indexOf(e.target.value)>=0)
+    })
+  }
   }
 
 
@@ -24,6 +47,13 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter 
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products}/>
             </div>
             <div className="sidebar">Cart Items</div>
