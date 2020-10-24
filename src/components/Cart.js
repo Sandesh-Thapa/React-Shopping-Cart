a67c8f0812b0097dbf4372c79231103a1b708fe9
Sandesh-Thapa <sandeshthapa426@gmail.com>
2020-10-24 18:49:45 +0545

    products backend

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from "react-reveal/Fade"
 
 export default class Cart extends Component {
     constructor(props){
@@ -37,6 +38,7 @@ export default class Cart extends Component {
                     )}
                 <div>
                     <div className="cart">
+                        <Fade left cascade={true}>
                         <ul className="cart-items">
                             {cartItems.map(item => (
                                 <li key={item._id}>
@@ -53,6 +55,7 @@ export default class Cart extends Component {
                                 </li>
                             ))}
                         </ul>
+                        </Fade>
                     </div>
                     {cartItems.length !== 0 && (
                         <div>
@@ -68,6 +71,7 @@ export default class Cart extends Component {
                             </div>
                         </div>
                         {this.state.showCheckout && (
+                            <Fade right cascade={true}>
                             <div className="cart">
                                 <form onSubmit={this.createOrder}>
                                     <ul className="form-container">
@@ -89,6 +93,7 @@ export default class Cart extends Component {
                                     </ul>
                                 </form>
                             </div>
+                            </Fade>
                         )}
                         </div>
                     )}
